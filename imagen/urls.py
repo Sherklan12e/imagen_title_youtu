a458eaad4546d9c9eb13a7febd0a6e9961d6fbf8
Sherklan <97853791+Sherklan12e@users.noreply.github.com>
2024-11-28 17:09:14 -0300

    Eliminamos todo

--- imagen/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import index, eliminar
-urlpatterns = [
-    path('', index , name='index'),
-    path('eliminar/<int:idimagen>/' , eliminar ,name='eliminar'),
-]
