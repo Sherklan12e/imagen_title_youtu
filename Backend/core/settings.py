2ed43ddc712ff6217a26fd682011f445342bb155
Sherklan <97853791+Sherklan12e@users.noreply.github.com>
2024-11-29 07:51:49 -0300

    changes

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -127,3 +127,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+AUTH_USER_MODEL = 'users.User'
--- Backend/users/models.py
+++ Backend/users/models.py
@@ -4,3 +4,18 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     
+    # Especificamos related_name único para evitar conflictos
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
